c3d99423f3784f732a2af77e20c78826410c9c9e
ChabanovX <firstchabanov@gmail.com>
2024-11-06 14:20:05 +0300

    Initial Commit

---
+++ MarkdownNotes/MarkdownNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownNotesApp.swift
+//  MarkdownNotes
+//
+//  Created by Иван Чабанов on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownNotesTests/MarkdownNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownNotesTests.swift
+//  MarkdownNotesTests
+//
+//  Created by Иван Чабанов on 06/11/2024.
+//
+
+import Testing
+@testable import MarkdownNotes
+
+struct MarkdownNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
